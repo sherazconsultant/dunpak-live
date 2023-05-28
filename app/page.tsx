fa92ae5e60dc216be88327b87773867783cfa64d
sherazconsultant <sheraz.it.consultant@gmail.com>
2023-05-28 13:04:26 +0500

    fb link added

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,13 @@ export default function Home() {
         height={75}
         priority
       />
-      <div className=' text-sm sm:text-2xl lg:text-2xl p-5 from-neutral-300 '>
-        Under construction
+      <div className='text-sm sm:text-2xl lg:text-2xl p-5 from-neutral-300'>
+        <a
+          className='text-white-500 bg-blue-500'
+          href='https://www.facebook.com/profile.php?id=100093073745609'
+        >
+          Please visit this Facebook page
+        </a>
       </div>
     </main>
   );
